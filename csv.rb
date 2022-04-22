0e0aa676ada048245b8c16c9f202e8a27c50db01
Raskolnikov <jerry.tann@endress.com>
2022-04-22 14:40:42 +0800

    ✨ play with ruby exceptions

--- csv.rb
+++ csv.rb
@@ -3,6 +3,13 @@ require 'csv'
 file_path = "./example.csv"
 CSV.foreach(file_path, headers: true) do |line|
     puts "line is #{line}"
+    begin
+        raise "fu" if line.fetch("symbol") == "Wb"
+    rescue 
+        puts "skipping" 
+        next
+    end
     puts "line.size #{line.size}"
     puts "fetched symbol #{line.fetch("symbol")}"
+   
 end
